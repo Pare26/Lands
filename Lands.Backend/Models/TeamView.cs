715220cc9d8b9bdf105ba7766fd1bb2fdf15dcfe
cayman01 <cayman01.filiales@gmail.com>
2018-04-06 21:31:11 -0500

    Teams Controller

---
+++ Lands.Backend/Models/TeamView.cs
@@ -0,0 +1,10 @@
+﻿namespace Lands.Backend.Models
+{
+    using Lands.Domain;
+    using System.Web;
+
+    public class TeamView : Team
+    {
+        public HttpPostedFileBase ImageFile { get; set; }
+    }
+}
---
+++ Lands.Domain/Team.cs
@@ -0,0 +1,17 @@
+﻿namespace Lands.Domain
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Team
+    {
+        [Key]
+        public int TeamId { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is requiered.")]
+        [MaxLength(50, ErrorMessage = "The field {0} only can contains a maximum of {1} characters lenght.")]
+        public string Name { get; set; }
+
+        [Display(Name = "Image")]
+        public string ImagePath { get; set; }
+    }
+}
