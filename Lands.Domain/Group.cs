329023ce7ed3a2179fcf2bebfde4f1ca3d9504c5
cayman01 <cayman01.filiales@gmail.com>
2018-04-11 21:33:47 -0500

    Groups controller

--- Lands.Domain/Group.cs
+++ Lands.Domain/Group.cs
@@ -5,18 +5,15 @@
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
 
-    public class Team
+    public class Group
     {
         [Key]
-        public int TeamId { get; set; }
+        public int GroupId { get; set; }
 
         [Required(ErrorMessage = "The field {0} is requiered.")]
         [MaxLength(50, ErrorMessage = "The field {0} only can contains a maximum of {1} characters lenght.")]
-        [Index("Team_Name_Index", IsUnique = true)]
+        [Index("Group_Name_Index", IsUnique = true)]
         public string Name { get; set; }
-
-        [Display(Name = "Image")]
-        public string ImagePath { get; set; }
 
         [JsonIgnore]
         public virtual ICollection<GroupTeam> GroupTeams { get; set; }
