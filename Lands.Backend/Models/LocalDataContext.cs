1a437f3d1e1d59c9f2f565ec3990e89774c63523
cayman01 <cayman01.filiales@gmail.com>
2018-04-13 21:24:30 -0500

    Begin matches controller

--- Lands.Backend/Models/LocalDataContext.cs
+++ Lands.Backend/Models/LocalDataContext.cs
@@ -7,5 +7,7 @@
         public System.Data.Entity.DbSet<Lands.Domain.User> Users { get; set; }
 
         public System.Data.Entity.DbSet<Lands.Domain.UserType> UserTypes { get; set; }
+
+        public System.Data.Entity.DbSet<Lands.Domain.Match> Matches { get; set; }
     }
 }
--- Lands.Domain/DataContext.cs
+++ Lands.Domain/DataContext.cs
@@ -1,13 +1,11 @@
 ﻿namespace Lands.Domain
 {
     using System.Data.Entity;
+    using System.Data.Entity.ModelConfiguration.Conventions;
 
     public class DataContext : DbContext
     {
-        public DataContext() : base("DefaultConnection")
-        {
-        }
-
+        #region Properties
         public DbSet<User> Users { get; set; }
 
         public DbSet<UserType> UserTypes { get; set; }
@@ -17,5 +15,22 @@
         public DbSet<Group> Groups { get; set; }
 
         public DbSet<GroupTeam> GroupTeams { get; set; }
+
+        public DbSet<StatusMatch> StatusMatches { get; set; }
+        #endregion
+
+        #region Constructors
+        public DataContext() : base("DefaultConnection")
+        {
+        }
+        #endregion
+
+        #region Methods
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            modelBuilder.Configurations.Add(new MatchesMap());
+        }
+        #endregion
     }
 }
